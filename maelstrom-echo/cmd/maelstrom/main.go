cceb4b7306c1bb5abcf53281e08bc4c533d705a1
uttam282005 <uttam282005@gmail.com>
2026-09-17 00:31:54 +0530

    e

--- maelstrom-echo/cmd/maelstrom/main.go
+++ maelstrom-echo/cmd/maelstrom/main.go
@@ -2,24 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
-	"net/rpc"
-
-	internal "maelstrom-echo/internal/maelstrom-handlers"
+	"uuid"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	// connect to the local RPC server
-	client, err := rpc.Dial("tcp", "localhost:1234")
-	if err != nil {
-		log.Fatal("Dialing error:", err)
-	}
-	defer client.Close()
+	// client, err := rpc.Dial("tcp", "localhost:1234")
+	// if err != nil {
+	// 	log.Fatal("Dialing error:", err)
+	// }
+	// defer client.Close()
 
-	log.Println("connected to rpc server")
-	
+	// log.Println("connected to rpc server")
+
 	n := maelstrom.NewNode()
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
@@ -33,7 +32,19 @@ func main() {
 		return n.Reply(msg, body)
 	})
 
-	n.Handle("generate", internal.HandleGenerate(n, client))
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		body["id"] = fmt.Sprintf("%s-%s", n.ID(), uuid.New().String())
+
+		return n.Reply(msg, body)
+	})
+
+	// n.Handle("generate", internal.HandleGenerate(n, client))
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
